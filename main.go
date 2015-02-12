e7f8dee2ff44c2eb6f4aa96814297d9bb00fd8db
Robin Schroer <sulami@peerwire.org>
2015-02-12 13:37:09 +0100

    add basic logging and arg parsing

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
 
 import "github.com/sulami/odf_server/auth"
 
 func main() {
+	port := flag.Int("port", 1338, "Port to listen on")
+	flag.Parse()
+
+	fmt.Println(logtime(), "Using port:", *port)
 	user := auth.User{"sulami", "robin"}
-	fmt.Println(&user)
+	fmt.Println(logtime(), "User connected:", user)
+}
+
+func logtime() string {
+	return time.Now().Format("2006-01-02 15:05")
 }
 

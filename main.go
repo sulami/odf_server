ed3a9db13fda640c6a2417a9ade379a82e98f273
Robin Schroer <sulami@peerwire.org>
2015-02-12 14:11:30 +0100

    move log to separate package

--- main.go
+++ main.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"strconv"
-	"time"
 )
 
 import "github.com/sulami/odf_server/auth"
+import "github.com/sulami/odf_server/log"
 
 func main() {
 	port := flag.Int("port", 1338, "Port to listen on")
 	flag.Parse()
 
-	log("Using port: " + strconv.Itoa(*port))
-	user := auth.User{"sulami", "robin"}
-	log("User connected: " + user.Username)
-}
 
-func log(msg string) {
-	fmt.Println(time.Now().Format("2006-01-02 15:04"), msg)
+	log.Log("Using port: " + strconv.Itoa(*port))
+	user := auth.User{"sulami", "robin"}
+	log.Log("User connected: " + user.Username)
 }
 

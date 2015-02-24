b54d81665936d4bc868df4b40e04f13e71e0c6a8
Robin Schroer <sulami@peerwire.org>
2015-02-24 13:28:52 +0100

    select unclaimed homeworld for new players

--- game/player.go
+++ game/player.go
@@ -1,5 +1,10 @@
 package game
 
+import (
+	"math/rand"
+	"time"
+)
+
 type player struct {
 	name string
 	money int
@@ -12,8 +17,19 @@ func initPlayer(name string, universe []Sector) player {
 	p := player{
 		name: name,
 		money: 10000,
+		homeworld: selectHomeworld(universe),
 	}
-	// TODO select a homeworld
 	return p
+}
+
+func selectHomeworld(universe []Sector) *world {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	hs := universe[r.Intn(len(universe))]
+	hw := hs.worlds[r.Intn(len(hs.worlds))]
+	if hw.owner == nil {
+		return hw
+	} else {
+		return selectHomeworld(universe)
+	}
 }
 
